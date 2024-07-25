fc311112b457d00acf5d11228c981b8a302a6e41
dnlslcd <danielsalcedoq@gmail.com>
2024-07-25 20:21:24 +0200

    búsqueda por título - hecho

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // importar modulos de terceros
 const express = require('express');
 const morgan = require('morgan');
+const {getColorFromURL} = require('color-thief-node');
+
 // const { title } = require('process'); ???
 
 // creamos una instancia del servidor Express
@@ -13,7 +15,8 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 // base de datos de imagenes
-const images = [{
+const images = [
+    {
     title: "happy cat",
     url: "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg"
 }, {
@@ -25,7 +28,10 @@ const images = [{
 }, {
     title: "woman in lake",
     url: "https://images.pexels.com/photos/2365067/pexels-photo-2365067.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-}];
+}
+];
+
+
 
 // especificar a Express que quiero usar EJS como motor de plantillas 
 app.set('view engine', 'ejs');
@@ -46,12 +52,13 @@ app.get('/', (req, res) => {
 // Petición GET a '/add-image-form' --> renderizo add-image-form.ejs 
 app.get('/add-image-form', (req, res) => {
     res.render('new-image-form', {
-        imageIsAdded: undefined
+        imageIsAdded: undefined,
+        imageIsRepeated: undefined
     });
 })
 
 // Petición POST a '/add-image-form' --> recibo los datos del formulario y actualizo mi "base de datos"  
-app.post('/add-image-form', (req, res) => {
+app.post('/add-image-form', async (req, res) => {
     // todos los datos nos vienen en un req.body
     console.log(req.body);
 
@@ -59,60 +66,89 @@ app.post('/add-image-form', (req, res) => {
     const { title, url, date } = req.body;
 
     // OPCIONAL: validación del lado servidor de que realmente nos han enviado un title
-        // expresion para validar el formato del title
+    // expresión regular para validar el formato del title
         const titleRegex = /^[0-9A-Z\s_]+$/i;
+
     // si el title no cumple la expresion, lanzo un error
     if (title.length > 30 || !titleRegex.test(title)) {
         return res.status(400).send('Algo ha salido mal...');
     }
 
 
-        // opción 1 (sacar los campos):
-        images.push({
-        title,
-    // 3. añadir los otros campos del formulario y sus validaciones
-        url,
-        date
-    }); // opción 2: images.push(req.body);
+        
 
     // console.log('array de imagenes actualizado: ', images);
 
         // TO DO:
-    // tras insertar la imagen dejaremos el formulario visible
     // ordenar las fotografías por fecha de más reciente a más antigua
     images.sort((a, b) => new Date(b.date) - new Date(a.date));
 
 
-    res.render('new-image-form', {
-        imageIsAdded: true
-    }); // opción: res.send('Datos recibidos');
-
-})
+    // extraer color con el modulo color-thief-node:
+    const dominantColor = await getColorFromURL(url);
 
 
+    // comprobar si url está repetida:
+    const isRepeated = images.some(i => i.url.toLocaleLowerCase() == url.toLocaleLowerCase());
+    // si está repetida, lanzo un error
+    if (isRepeated){
+        res.render('new-image-form', {
+            imageIsAdded: false,
+            imageIsRepeated: url
+        });
+    } 
+    // si no está repetida, todo sigue correctamente
+    else {
+        // opción 1 (sacar los campos):
+        images.push({
+            title,
+        // 3. añadir los otros campos del formulario y sus validaciones
+            url,
+            date,
+            dominantColor
+        }); 
+        
+        // opción 2: images.push(req.body);
+
+
+        res.render('new-image-form', {
+            imageIsAdded: true,
+            imageIsRepeated: undefined
+        });
+    }
+    
+
+});
+
+/**
+ * 
+ * @param {string} s1 String principal. Cadena de texto donde vamos a realizar la búsqueda 
+ * @param {string} s2 String secundario.  
+ * @returns string Retorna true si s2 está contenido en s1. En caso contrario retorna false
+ */
+function isSubstring(s1, s2) {
+    const regexp = new RegExp(s2, "i");
+
+    // Busco en el string s1 si existe el string s2
+    return regexp.test(s1);
+}
 
 // OPCIONAL: nuevo endpoint para gestionar la búsqueda
 app.get('/search', (req, res) => {
     
     // 1. coger el valor del parametro keyword de la query string
-    const queryKeyword = document.querySelector('input[name="keyword"]');
-    const searchQuery = new URLSearchParams(window.location.search).get('keyword');
+    const queryKeyword = req.query.keyword;
+    // const searchQuery = new URLSearchParams(window.location.search).get('keyword');
 
     // 2. buscar en la base de datos usando el metodo filter
-    const filteredImages = images.filter(image => {
-        return image.title.toLowerCase().includes(searchQuery.toLowerCase());
-    });
+    const filteredImages = images.filter((image) => isSubstring(image.title, queryKeyword));
 
-    // para las mayusculas-minusculas, dos opciones:
-        // 1. usar el metodo toLowerCase() en el parametro de la query string o
-        // 2. usar una expresión regular
-        
+        // Otra opción para las mayus-minus: usar el metodo toLowerCase() en el parametro de la query string.
     
     // 3. usar res.render para renderizar la vista home.ejs y pasarle el array de imagenes filtrado
     res.render('home',{
         images: filteredImages
-    }
-    );
+    });
 });
 
 
